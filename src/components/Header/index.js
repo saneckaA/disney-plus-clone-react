02b3a3debeadb7a347db9ac5c4e64bdb6b982f34
Aleksandra Sanecka <a.saneckaa@gmail.com>
2023-06-21 15:50:22 +0200

    ADD: header

---
+++ src/components/Header/index.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Nav, Logo, NavMenu, UserImg } from './styled';
+
+
+function Header() {
+    return (
+        <Nav>
+            <Logo src="/images/logo.svg" />
+            <NavMenu>
+                <a>
+                    <img src="/images/home-icon.svg" />
+                    <span>HOME</span>
+                </a>
+                <a>
+                    <img src="/images/search-icon.svg" />
+                    <span>SEARCH</span>
+                </a>
+                <a>
+                    <img src="/images/watchlist-icon.svg" />
+                    <span>WATCHLIST</span>
+                </a>
+                <a>
+                    <img src="/images/original-icon.svg" />
+                    <span>ORIGINALS</span>
+                </a>
+                <a>
+                    <img src="/images/movie-icon.svg" />
+                    <span>MOVIES</span>
+                </a>
+                <a>
+                    <img src="/images/series-icon.svg" />
+                    <span>SERIES</span>
+                </a>
+            </NavMenu>
+            <UserImg src="/images/user-image.jpg"/>
+        </Nav>
+    )
+}
+
+export default Header;
