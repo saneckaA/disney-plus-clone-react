213da9fdd21c69bb59322abd4751cfd656a3fa5b
Aleksandra Sanecka <a.saneckaa@gmail.com>
2023-07-06 17:47:13 +0200

    ADD: videos to viewers on hover

--- src/components/Viewers/index.js
+++ src/components/Viewers/index.js
@@ -3,27 +3,47 @@ import { Container, Wrap } from './styled';
 
 
 function Viewers
-() {
+  () {
   return (
     <Container>
-       <Wrap>
+      <Wrap>
         <img src="/images/viewers-disney.png" />
-       </Wrap>
-       <Wrap>
+        <video autoPlay loop playsInline muted
+          src="/videos/1564674844-disney.mp4" type="video/mp4" >
+        </video>
+      </Wrap>
+      <Wrap>
         <img src="/images/viewers-pixar.png" />
-       </Wrap>
-       <Wrap>
+        <video autoPlay={true} loop={true} playsInline={true} muted
+          src="/videos/1564676714-pixar.mp4" type="video/mp4">
+
+        </video>
+      </Wrap>
+      <Wrap>
         <img src="/images/viewers-marvel.png" />
-       </Wrap>
-       <Wrap>
+        <video autoPlay={true} loop={true} playsInline={true} muted
+          src="/videos/1564676115-marvel.mp4" type="video/mp4" >
+
+        </video>
+      </Wrap>
+      <Wrap>
         <img src="/images/viewers-starwars.png" />
-       </Wrap>
-       <Wrap>
+        <video autoPlay={true} loop={true} playsInline={true} muted
+          src="/videos/1608229455-star-wars.mp4" type="video/mp4">
+        </video>
+      </Wrap>
+      <Wrap>
         <img src="/images/viewers-national.png" />
-       </Wrap>
-       <Wrap>
+        <video autoPlay={true} loop={true} playsInline={true} muted
+          src="/videos/1564676296-national-geographic.mp4" type="video/mp4">
+        </video>
+      </Wrap>
+      <Wrap>
         <img src="/images/viewers-star.png" />
-       </Wrap>
+        <video autoPlay={true} loop={true} playsInline={true} muted
+          src="/videos/1608169994-brand-star.mp4" type="video/mp4" >
+        </video>
+      </Wrap>
     </Container>
   )
 }
--- src/components/Viewers/styled.js
+++ src/components/Viewers/styled.js
@@ -6,29 +6,54 @@ export const Container = styled.div`
    grid-template-columns: repeat(6, 190px);
    grid-gap: 20px;
    justify-content: center;
-
 `;
 
 export const Wrap = styled.div`
-background-image: linear-gradient(#2b2c45, #151521);
 padding: 20px;
-display: flex;
-align-items: center;
-justify-content: center;
 border-radius: 8px;
 border:  3px solid rgba(249, 249, 249, 0.3);
 cursor: pointer;
 height: 100px;
+overflow: hidden;
+position: relative;
 box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
     rgb(0 0 0 / 73%) 0px 16px 10px -10px;
 transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
 
-    &:hover {
-transform: scale(1.1);
-border:  3px solid rgba(249, 249, 249, 0.8);
+    img {
+        inset: 0px;
+        display: block;
+        height: 100%;
+        object-fit: cover;
+        opacity: 1;
+        position: absolute;
+        transition: opacity 500ms ease-in-out 0s;
+        width: 100%;
+        z-index: 1;
+        top: 0;
     }
 
-    img {
-        height: 100px;
+    video {
+        width: 50%;
+        height: 50%;
+        min-width: 100%;
+        min-height: 100%;
+        overflow: hidden;
+        object-fit: cover;
+        position: absolute;
+        right: 0;
+        bottom: 0;
+        top: 0px;
+        opacity: 0;
+        z-index: 0;
+    }
+
+    &:hover {
+      transform: scale(1.1);
+      border: 3px solid rgba(249, 249, 249, 0.8);
+
+      video {
+        opacity: 1;
+      }
     }
 `;
