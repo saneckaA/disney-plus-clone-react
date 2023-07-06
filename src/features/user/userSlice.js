c51bcded40b8886db5a6e96e279a87feab5869dc
Aleksandra Sanecka <a.saneckaa@gmail.com>
2023-07-06 17:47:53 +0200

    ADD: movie and user slice

---
+++ src/features/user/userSlice.js
@@ -0,0 +1,29 @@
+import  {createSlice} from '@reduxjs/toolkit';
+
+const initialState = {
+    name: '',
+    email: '',
+};
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        setUserLoginDetails: (state, action) => {
+            state.name = action.payload.name;
+            state.email = action.payload.email;
+        },
+
+        setSignOutState: state => {
+            state.name = null;
+            state.email = null;
+        }
+    },
+});
+
+export const {setUserLoginDetails, setSignOutState} = userSlice.actions;
+
+export const selectUserName = state => state.user.name;
+export const selectUserEmail = state => state.user.email;
+
+export default userSlice.reducer;
